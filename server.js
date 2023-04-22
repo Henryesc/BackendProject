6d894b0b67ff78f62ea38177633dd7bc8d9a1833
Luis Martinez <rickymtz1999@gmail.com>
2023-04-22 10:06:07 -0500

    set PORT as environemnt variable and add .env in ignore

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const server = express();
+
+/* `const { PORT } = process.env;` is destructuring the `PORT` property from the `process.env` object.
+This is a common way to access environment variables in Node.js. The `process.env` object contains
+all the environment variables defined in the current environment, and by destructuring the `PORT`
+property, we can access its value and use it in our code. */
+const PORT = process.env.PORT || 8080;
 
 server.get('/heartbeat', (req, res) => {
     res.json({
@@ -9,5 +17,5 @@ server.get('/heartbeat', (req, res) => {
 });
 
 server.listen (8080, () => {
-    console.log('The server is running at PORT 8080')
+    console.log(`The server is running at PORT ${PORT}.`)
 });
